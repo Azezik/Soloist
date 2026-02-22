273365a3a5aee7a6faa7c90e34eaa85d3995b06e
Azezik <pandanoodlesoup@gmail.com>
2026-02-22 12:53:19 -0500

    Remove trailing newlines from Open in mail body

--- src/app.js
+++ src/app.js
@@ -1682,11 +1682,12 @@ async function renderLeadDetail(leadId) {
     }
     const subject = String(mailButton?.dataset.mailSubject || "");
     const body = String(mailButton?.dataset.mailBody || "");
+    const bodyWithoutTrailingNewlines = body.replace(/\n+$/, "");
     const mailtoParams = new URLSearchParams();
     if (subject.trim()) {
       mailtoParams.set("subject", subject);
     }
-    mailtoParams.set("body", body);
+    mailtoParams.set("body", bodyWithoutTrailingNewlines);
     const mailtoQuery = mailtoParams.toString().replace(/\+/g, "%20");
     const mailtoUrl = `mailto:${to}?${mailtoQuery}`;
     window.location.href = mailtoUrl;
